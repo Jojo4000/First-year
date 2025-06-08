81689a57b23fdeeb53852e54402a9bd0d47d2443
Jojo4000 <122701250+Jojo4000@users.noreply.github.com>
2025-06-08 19:44:37 +0100

    Delete streamflix_codigo_exe_2304353efolioA directory

--- streamflix_codigo_exe_2304353efolioA/include/user_login.h
+++
@@ -1,9 +0,0 @@
-#ifndef USER_LOGIN_H
-#define USER_LOGIN_H
-
-#include "..\include\structs.h"
-
-// Permite o utilizador fazer login e criar uma conta se ainda não existir
-user *login_user(user **users, int *user_count);
-
-#endif // USER_LOGIN_H
